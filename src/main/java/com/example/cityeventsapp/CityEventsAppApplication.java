2b4af4c1c0f91349b16c31b2b412dfd405f130fd
arturrebarppz2024 <artur.rebar.ppz.2024@lpnu.ua>
2026-01-06 01:04:29 +0200

    Initial commit: Spring Boot + PostgreSQL setup

---
+++ src/main/java/com/example/cityeventsapp/CityEventsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityeventsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityEventsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityEventsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cityeventsapp/CityEventsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityeventsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityEventsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
